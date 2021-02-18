6c41dccc7f03cbe5d43c3930b02a9b1dbfd56eef
SAMSMU\dmakarichev <d.a.makarychev@samsmu.ru>
2021-02-18 13:54:55 +0400

    queue visualised now

--- Assets/Scripts/UI/QueueCell.cs
+++ Assets/Scripts/UI/QueueCell.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class QueueCell : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer m_QueueObjectSpriteRenderer;
+    [SerializeField] private GameObject m_ActiveCell;
+
+    public GameObject ActiveCell
+    {
+        get => m_ActiveCell;
+        set => m_ActiveCell = value;
+    }
 
     public void SetSprite(Sprite queueObjectSprite)
     {
