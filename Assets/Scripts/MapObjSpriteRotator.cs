e58643553bbe84518519a0ca2d8811b72a269de3
SAMSMU\dmakarichev <d.a.makarychev@samsmu.ru>
2021-02-26 09:42:04 +0400

    hotfix

---
+++ Assets/Scripts/MapObjSpriteRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjSpriteRotator : MonoBehaviour
+{
+    [SerializeField] private Transform m_Sprite;
+    public void RotateSprite(Vector2Int input)
+    {
+        if(input == Vector2Int.right)
+            m_Sprite.rotation = Quaternion.Euler(0,0,0);
+        else if (input == Vector2Int.left)
+        {
+            m_Sprite.rotation = Quaternion.Euler(0,180,0);
+        }
+    }
+}
