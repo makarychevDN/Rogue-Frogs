a98b07973cecbf0f98f2326a220017ba1dc982c6
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-04-13 23:25:31 +0400

    added show and hide character UI script

---
+++ Assets/Scripts/UI/ShowUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> m_UiObjects;
+
+    private void Start()
+    {
+        foreach (var temp in m_UiObjects)
+        {
+            temp.SetActive(false);
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        foreach (var temp in m_UiObjects)
+        {
+            temp.SetActive(true);
+        }
+    }
+    
+    void OnMouseExit()
+    {
+        foreach (var temp in m_UiObjects)
+        {
+            temp.SetActive(false);
+        }
+    }
+}
