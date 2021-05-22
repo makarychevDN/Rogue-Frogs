fe8c45855ba4f6bb27141a3a2c5f0c7bdb669ec8
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-05-22 23:19:00 +0400

    animation speed settings module (wip)

--- Assets/Scripts/SkipTurnModule.cs
+++ Assets/Scripts/SkipTurnModule.cs
@@ -7,19 +7,26 @@ public class SkipTurnModule : MonoBehaviour
     [Header("References Setup")] 
     [SerializeField] private MapObject m_MapObject;
     [SerializeField] private GameObject m_SkipTurnAnimationGameObject;
-    private float m_SkipTurnAnimationTime = 0.73f;
+    static float m_SkipTurnAnimationTime = 0.73f;
+    static float m_SkipTurnDelay = 0.73f;
+
+    public static float SkipTurnDelay { get => m_SkipTurnDelay; set => m_SkipTurnDelay = value; }
 
     public void SkipTurn()
     {
         m_MapObject.ActiveObjectsQueue.AddToActiveObjectsList(this);
         m_SkipTurnAnimationGameObject.SetActive(true);
-        Invoke("SendMessageToQueue", m_SkipTurnAnimationTime);
+        Invoke("StopClockAnimation", m_SkipTurnAnimationTime);
+        Invoke("SendMessageToQueue", m_SkipTurnDelay);
     }
 
     private void SendMessageToQueue()
     {
         m_MapObject.ActiveObjectsQueue.RemoveFromActiveObjectsList(this);
-        m_SkipTurnAnimationGameObject.SetActive(false);
         m_MapObject.ActiveObjectsQueue.SkipTheTurn();
+    }
+    private void StopClockAnimation()
+    {
+        m_SkipTurnAnimationGameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/UI/SkipTurnTimeSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkipTurnTimeSettings : MonoBehaviour
+{
+    [SerializeField] private Slider m_SkipTurnTime;
+    [SerializeField] private Slider m_SkipTurnAnimation;
+    [SerializeField] private bool m_;
+}
