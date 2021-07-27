73234fd8619c5e304d802c17fae6dabf8f145cae
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2021-07-27 23:57:00 +0400

    new architecture concept

---
+++ Assets/Scripts/ActivatableBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivatableBehavior : MonoBehaviour
+{
+    protected bool ActiveNow 
+    {
+        set 
+        {
+            if (value)
+                CurrentlyActiveObjects.Add(this);
+            else
+                CurrentlyActiveObjects.Remove(this);
+        }
+    }
+}
